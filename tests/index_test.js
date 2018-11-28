eac7185b87ab96d572a120c4834e3f2b657c7e9f
Maciek Sakrejda <m.sakrejda@gmail.com>
2018-11-28 12:35:50 -0800

    React 16 (#6)
    Move react-malibu to react-16

--- tests/index_test.js
+++ tests/index_test.js
@@ -4,6 +4,11 @@ import { beforeEach, before, after } from 'mocha'
 import sinon from 'sinon'
 import { jsdom } from 'jsdom'
 
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
+
 // Since react will console.error propType warnings, that which we'd rather have
 // as errors, we use sinon.js to stub it into throwing these warning as errors
 // instead.
@@ -15,11 +20,14 @@ after(() => { console.error.restore() })
 
 // setup jsdom
 beforeEach(() => {
+  const fetch = window.fetch
   jsdom.env({
     url: 'https://data.example.com/',
     html: '<div></div>',
     created: (err, window) => {
       if (!err) {
+        window.fetch = fetch
+        global.fetch = fetch
         global.window = window
       }
     }
